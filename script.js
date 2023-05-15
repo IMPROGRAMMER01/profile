0cb77794a86ae06462b7988d7b187f83f0d3b22b
IMPROGRAMMER01 <133694663+IMPROGRAMMER01@users.noreply.github.com>
2023-05-15 23:27:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","Web Devloper","Web Designer","Software Engineer","Vedio Editor","Youtuber","Graphics Designer"],
+    typespeed:10,
+    Bcakspeed:20,
+    loop:true
+})
